7da3ea02b26733b1efb3fc19c49b2f932cff78b1
Bopamo Osaisai <bopamp@gmail.com>
2019-09-05 10:54:55 -0700

    Adding requirements and running black on podcast project files
    Signed-off-by: Bopamo Osaisai <bopamo@gmail.com>

--- demos/projects/podcast/program.py
+++ demos/projects/podcast/program.py
@@ -18,12 +18,12 @@ def show_header():
 def display_results():
 
     start = random.randint(90, 110)
-    end = random.randint(130, service.get_latest_show_id()+1)
+    end = random.randint(130, service.get_latest_show_id() + 1)
 
     for show_id in range(start, end):
         info = service.get_episode(show_id)
         print("{}. {}".format(info.show_id, info.title))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
